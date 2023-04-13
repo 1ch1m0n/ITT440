f8bf79a149920423dbe43a83c2e4a1b4372e9837
1ch1m0n <129719310+1ch1m0n@users.noreply.github.com>
2023-04-13 06:01:48 -0400

    Add files via upload

---
+++ Lab 3/Lab 3.1 - IPC Signals.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+int main(void)
+{
+   void sigint_handler(int a);
+   void sigtstp_handler(int b);
+   void sigquit_handler(int c);
+   char s[200];
+
+   if (signal(SIGINT, sigint_handler) == SIG_ERR){
+      perror("signal");
+      exit(1);
+   }
+   if (signal(SIGTSTP,sigtstp_handler) == SIG_ERR){
+      perror("signal");
+      exit(1);
+   }
+   if (signal(SIGQUIT,sigquit_handler) == SIG_ERR){
+      perror("signal");
+      exit(1);
+   }
+   printf("Enter a string:\n");
+   if (fgets(s, 200, stdin) == NULL){
+      perror("gets");
+      perror("gets");
+      perror("gets");
+   }
+   
+ return 0;
+}
+void sigint_handler(int a)
+{
+   printf("This is a special handler for SIGINT \n");
+}
+void sigtstp_handler(int b)
+{
+   printf("This is a special handler for SIGTSTP \n");
+}
+void sigquit_handler(int c)
+{
+   printf("This is a special handler for SIGQUIT \n");
+}
